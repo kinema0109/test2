e9c9942f2962534d07f03ff82ec705edc0d8185f
Hoang Cong Tho <thoqb123456@gmail.com>
2022-08-24 14:49:12 +0700

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  
+  resources :microposts
+  resources :users
   get 'about', to: 'pages#about'
   get 'home', to: 'pages#home'
-  root 'pages#home'
+  #root "application#hello"
+  root "users#index"
+  #root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
