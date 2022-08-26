37d90730b1488e696463de1b6ccdcb6ae6722bdb
Hoang Cong Tho <thoqb123456@gmail.com>
2022-08-26 10:18:22 +0700

    layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home
+  def contract
   end
 
-  def about
+  def signup
   end
 
-  def help
+  def login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+  get 'users/new'
+  get '/contact',to: 'static_pages#contact'
+  get '/signup',to: 'users#new'
+  get 'static_pages/login'
   get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/about',to: 'static_pages#about'
+  get '/help',to: 'static_pages#help'
   resources :microposts
   resources :users
   get 'about', to: 'pages#about'
