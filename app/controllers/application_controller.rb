0472baefa14c114ccea66cfc7406c5806d679cde
Hoang Cong Tho <thoqb123456@gmail.com>
2022-09-08 15:42:50 +0700

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  
   before_action :set_locale
-    def hello
-        render html:" el mundo"
-    end
-    def show
-      @user= User.find(params[:id])
-    end 
-    def new
-      @user=User.new
-    end 
-    def create
-      @user=User.new(user_params)
-      if @user.save
-      else 
-        render 'new'
-      end
-    end
+
     def default_url_options
       {locale: I18n.locale}
     end
