2c26b3489d4d7845b7c591a1cc38bcb1b89ff0f2
kinema0109 <76434880+kinema0109@users.noreply.github.com>
2022-09-13 09:57:18 +0700

    login (#8)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user=User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "User created successfully"
+      flash[:success] = t("flash.success")
       redirect_to @user
     else 
       render 'new'
